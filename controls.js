1ba7f6fd32386fe37c4939ce307eec3dad8d84e7
qinbinhua601 <qinbinhua601@hotmail.com>
2024-01-01 22:59:06 +0800

    feat: infinate scroll horizontally & add control button

--- controls.js
+++ controls.js
@@ -3,5 +3,25 @@ const $backToOriginButton = document.querySelector('#back-to-origin-button');
 $backToOriginButton.addEventListener('click', function() {
   lastScrollX = 0;
   lastScrollY = 0;
+  $lastScrollXSpan.innerText = 0;
   redrawChart(true);
 })
+
+const $clearButton = document.querySelector('#clear-button');
+
+$clearButton.addEventListener('click', function() {
+  const oldTaskLength = tasks.length;
+  tasks.length = 0;
+  for(let i = 0; i < oldTaskLength; i++) {
+    tasks.push({});
+  }
+  redrawChart(true);
+})
+
+const $clearMilestoneButton = document.querySelector('#clear-milestone-button');
+
+$clearMilestoneButton.addEventListener('click', function() {
+  mileStones.length = 0;
+  redrawChart(true);
+})
+
