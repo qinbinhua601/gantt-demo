896958e1be556ad7955d06488b1e8b7deecedac8
qinbinhua601 <qinbinhua601@hotmail.com>
2024-01-01 23:02:27 +0800

    feat: hide all control buttons

--- controls.js
+++ controls.js
@@ -1,6 +1,6 @@
 const $backToOriginButton = document.querySelector('#back-to-origin-button');
 
-$backToOriginButton.addEventListener('click', function() {
+$backToOriginButton?.addEventListener('click', function() {
   lastScrollX = 0;
   lastScrollY = 0;
   $lastScrollXSpan.innerText = 0;
@@ -9,7 +9,7 @@ $backToOriginButton.addEventListener('click', function() {
 
 const $clearButton = document.querySelector('#clear-button');
 
-$clearButton.addEventListener('click', function() {
+$clearButton?.addEventListener('click', function() {
   const oldTaskLength = tasks.length;
   tasks.length = 0;
   for(let i = 0; i < oldTaskLength; i++) {
@@ -20,7 +20,7 @@ $clearButton.addEventListener('click', function() {
 
 const $clearMilestoneButton = document.querySelector('#clear-milestone-button');
 
-$clearMilestoneButton.addEventListener('click', function() {
+$clearMilestoneButton?.addEventListener('click', function() {
   mileStones.length = 0;
   redrawChart(true);
 })
