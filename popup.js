2aa956448cac4e5382f038d5ebd8aca46c936cbf
qinbinhua601 <qinbinhua601@hotmail.com>
2024-01-03 23:35:08 +0800

    feat: add copy button for duplicate taskBar on the next line

--- popup.js
+++ popup.js
@@ -7,6 +7,7 @@ Popup.show = function () {
     const $popupBody = document.querySelector('.popup-wrapper__body');
     const $button = $popup.querySelector('button.modify');
     const $deleteButton = $popup.querySelector('button.delete');
+    const $copyButton = $popup.querySelector('button.copy');
 
     $popup.addEventListener('click', function () {
       Popup.hide();
@@ -29,6 +30,15 @@ Popup.show = function () {
         tasks.splice(index, 1)
         syncLocal();
       }
+      Popup.hide();
+      redrawChart(true);
+    }
+
+    $copyButton.onclick = function (e) {
+      e.preventDefault();
+      const index = this.parentElement.previousElementSibling.dataset.index;
+      const curTask = {...tasks[index]};
+      tasks.splice(index, 0, curTask);
       Popup.hide();
       redrawChart(true);
     }
