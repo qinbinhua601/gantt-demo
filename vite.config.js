05c3e94b5b4a646a59d318ce15a211329209df75
qinbinhua601 <qinbinhua601@hotmail.com>
2024-01-06 19:39:24 +0800

    chore: server.open = true

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gantt-demo/"
+  base: "/gantt-demo/",
+  server: {
+    open: true
+  }
 })
